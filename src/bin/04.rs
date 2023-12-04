33e34c28a032461e16749048bbff3df4d78a8059
Alex Schneider <alexschneider@netflix.com>
2023-12-04 08:22:32 -0800

    More clean up

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -37,12 +37,11 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let mut next_copies: HashMap<usize, usize> = HashMap::new();
     for (card_num, line) in input.lines().enumerate() {
-        let current_copies = *next_copies.get(&card_num).unwrap_or(&1);
-        next_copies.insert(card_num, current_copies);
+        let current_copies = *next_copies.entry(card_num).or_insert(1);
 
-        let num_winners: usize = find_winners(line);
+        let num_winners = find_winners(line);
         for card in card_num + 1..card_num + num_winners + 1 {
-            next_copies.insert(card, next_copies.get(&card).unwrap_or(&1) + current_copies);
+            *next_copies.entry(card).or_insert(1) += current_copies;
         }
     }
     next_copies.values().sum::<usize>().try_into().ok()
