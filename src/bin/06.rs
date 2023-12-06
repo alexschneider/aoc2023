b299f4078ef536e9d78369883f3d2fa0b5f07e30
Alex Schneider <alexschneider@netflix.com>
2023-12-06 07:21:18 -0800

    More functional now

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -8,19 +8,13 @@ pub fn part_one(input: &str) -> Option<u32> {
         .map(str::split_ascii_whitespace)
         .map(|i| i.skip(1))
         .map(|i| i.map(|s| s.parse::<u32>().unwrap()))
-        .collect_tuple()
-        .unwrap();
-    let mut ans = 1;
-    for (time, distance) in times.zip(distances) {
-        let mut successes = 0;
-        for i in 0..time {
-            if i * (time - i) > distance {
-                successes += 1;
-            }
-        }
-        ans *= successes;
-    }
-    Some(ans)
+        .collect_tuple()?;
+    Some(
+        times
+            .zip(distances)
+            .map(|(t, d)| (1..t).filter(|i| i * (t - i) > d).count() as u32)
+            .product(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -36,15 +30,8 @@ pub fn part_two(input: &str) -> Option<u32> {
                 .parse::<u128>()
                 .unwrap()
         })
-        .collect_tuple()
-        .unwrap();
-    let mut successes = 0;
-    for i in 0..time {
-        if i * (time - i) > distance {
-            successes += 1;
-        }
-    }
-    Some(successes)
+        .collect_tuple()?;
+    Some((1..time).filter(|i| i * (time - i) > distance).count() as u32)
 }
 
 #[cfg(test)]
