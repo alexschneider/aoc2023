c776894be11114a95aa928b8644a8a4e407345fd
Alex Schneider <alexschneider@netflix.com>
2023-12-07 21:54:22 -0800

    Day 8

---
+++ src/bin/08.rs
@@ -0,0 +1,90 @@
+use std::collections::HashMap;
+
+use num::Integer;
+
+advent_of_code::solution!(8);
+
+type NodeMap<'a> = HashMap<&'a str, (&'a str, &'a str)>;
+
+fn parse_input(input: &str) -> (impl Iterator<Item = char> + Clone + '_, NodeMap) {
+    let mut lines = input.lines();
+    let instructions = lines.next().unwrap().chars().cycle();
+
+    lines.next();
+    let nodes = lines
+        .map(|line| {
+            let (node, leafs) = line.split_once(" = ").unwrap();
+            let (l_leaf, r_leaf) = leafs
+                .trim_matches(|ch: char| ch == '(' || ch == ')')
+                .split_once(", ")
+                .unwrap();
+            (node, (l_leaf, r_leaf))
+        })
+        .collect::<NodeMap>();
+    (instructions, nodes)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut instructions, nodes) = parse_input(input);
+    let mut steps = 0;
+    let mut current_node = "AAA";
+
+    while current_node != "ZZZ" {
+        let (l_leaf, r_leaf) = nodes.get(current_node).unwrap();
+        let next_instruction = instructions.next().unwrap();
+        if next_instruction == 'L' {
+            current_node = l_leaf;
+        } else {
+            current_node = r_leaf;
+        }
+        steps += 1;
+    }
+    Some(steps)
+}
+
+pub fn part_two(input: &str) -> Option<u128> {
+    let (instructions, nodes) = parse_input(input);
+    let starting_nodes = nodes
+        .keys()
+        .filter(|k| k.ends_with('A'))
+        .cloned()
+        .collect::<Vec<&str>>();
+
+    let starting_node_counts = starting_nodes.iter().map(|&s| {
+        let mut current_instructions = instructions.clone();
+        let mut current_node = s;
+        let mut steps = 0;
+        while !current_node.ends_with('Z') {
+            let (l_leaf, r_leaf) = nodes.get(current_node).unwrap();
+            let next_instruction = current_instructions.next().unwrap();
+            if next_instruction == 'L' {
+                current_node = l_leaf;
+            } else {
+                current_node = r_leaf;
+            }
+            steps += 1;
+        }
+        steps
+    });
+
+    Some(starting_node_counts.fold(1, |acc, x| acc.lcm(&x)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(6));
+    }
+}
